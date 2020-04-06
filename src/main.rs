1aa420edd68211241e52ee89de50d18861d1df0a
Balázs Dukai <balazs.dukai@gmail.com>
2020-04-06 22:07:35 +0200

    Chapter 3

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
+mod vec;
+mod ray;
+use crate::vec::Vec3;
+use crate::ray::Ray;
+
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction: Vec3 = r.direction.unit_vector();
+    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+    &((1.0_f32 - t) * &Vec3::new(1.0, 1.0, 1.0)) + &(t * &Vec3::new(0.5, 0.7, 1.0))
+}
+
+fn main() -> std::io::Result<()> {
+    let mut f = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("basic.ppm")?;
+    let nx: i16 = 200;
+    let ny: i16 = 100;
+
+    // let col = Vec3::new(1.0, 1.0, 1.0);
+
+    f.write_all("P3\n".as_bytes());
+    f.write_all((format!("{} {}\n", nx, ny)).as_bytes());
+    f.write_all((format!("255\n")).as_bytes());
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+    for j in (0..ny).rev() {
+        for i in (0..nx) {
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let r = Ray::new(origin, &lower_left_corner + &(&(u*&horizontal) + &(v*&vertical)));
+            let col = color(&r);
+            let ir: i16 = (255.99 * col.e[0]) as i16;
+            let ig: i16 = (255.99 * col.e[1]) as i16;
+            let ib: i16 = (255.99 * col.e[2]) as i16;
+            f.write_all((format!("{} {} {}\n", ir, ig, ib)).as_bytes());
+        }
+    }
+    Ok(())
+}
---
+++ src/vec.rs
@@ -0,0 +1,121 @@
+use std::ops::{Add, AddAssign, Sub, Mul, Div};
+
+
+// Vec3
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3 {
+    pub e: [f32; 3]
+}
+
+impl Vec3 {
+    pub fn new(e0: f32, e1: f32, e2: f32) -> Vec3 {
+        Vec3 {
+            e: [e0, e1, e2]
+        }
+    }
+
+    pub fn x(&self) -> f32 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f32 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f32 {
+        self.e[2]
+    }
+
+    pub fn r(&self) -> f32 {
+        self.e[0]
+    }
+
+    pub fn g(&self) -> f32 {
+        self.e[1]
+    }
+
+    pub fn b(&self) -> f32 {
+        self.e[2]
+    }
+
+    pub fn length(&self) -> f32 {
+        (self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]).sqrt()
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        self / self.length()
+    }
+
+}
+
+impl Add for &Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Self) -> Self::Output {
+        Vec3::new(
+            self.e[0] + other.e[0],
+            self.e[1] + other.e[1],
+            self.e[2] + other.e[2]
+        )
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [
+                self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2]
+            ]
+        };
+    }
+}
+
+impl Mul<f32> for &Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vec3 { e: [
+            self.e[0] * rhs,
+            self.e[1] * rhs,
+            self.e[2] * rhs
+        ] }
+    }
+}
+
+impl Mul<&Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: &Vec3) -> Self::Output {
+        Vec3 { e: [
+            self * rhs.e[0],
+            self * rhs.e[1],
+            self * rhs.e[2]
+        ] }
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: Vec3) -> Self::Output {
+        Vec3 { e: [
+            self.e[0] / rhs.e[0],
+            self.e[1] / rhs.e[1],
+            self.e[2] / rhs.e[2]
+        ] }
+    }
+}
+
+impl Div<f32> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vec3 { e: [
+            self.e[0] / rhs,
+            self.e[1] / rhs,
+            self.e[2] / rhs
+        ] }
+    }
+}
