515ed03f5bf1b43c325c883ee8761b40261b28b5
Balázs Dukai <balazs.dukai@gmail.com>
2020-04-13 20:06:13 +0200

    Chapter 5: multiple spheres

---
+++ src/hitable.rs
@@ -0,0 +1,85 @@
+use crate::vec::{Vec3, dot};
+use crate::ray::Ray;
+
+pub struct HitRecord {
+    pub t: f32,
+    pub p: Vec3,
+    pub normal: Vec3,
+    pub on_edge: bool
+}
+
+impl HitRecord {
+    pub fn new() -> HitRecord {
+        HitRecord {
+            t: 0.0,
+            p: Vec3::new(0.0, 0.0, 0.0),
+            normal: Vec3::new(0.0, 0.0, 0.0),
+            on_edge: false
+        }
+    }
+}
+
+pub trait Hitable {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
+}
+
+pub struct Sphere {
+    pub center: Vec3,
+    pub radius: f32
+}
+
+impl Hitable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let oc: Vec3 = &r.origin - &self.center; // origin coordinate
+        let a: f32 = dot(&r.direction, &r.direction);
+        let b: f32 = 2.0 * dot(&oc, &r.direction);
+        let c: f32 = dot(&oc, &oc) - &self.radius * &self.radius;
+        let discriminant: f32 = b*b - 4_f32*a*c;
+        if discriminant > 0.0 {
+            let mut hitpoint = (-b - discriminant.sqrt()) / (2.0 * a);
+            if hitpoint < t_max && hitpoint > t_min {
+                let t: f32 = hitpoint;
+                let p: Vec3 = r.point_at_parameter(t);
+                let normal: Vec3 = &(&p - &self.center) / self.radius;
+                if discriminant < 0.0005 {
+                    return Some(HitRecord{t, p, normal, on_edge: true});
+                }
+                else {
+                    return Some(HitRecord{t, p, normal, on_edge: false});
+                }
+            }
+            hitpoint = (-b + discriminant.sqrt()) / (2.0 * a);
+            if hitpoint < t_max && hitpoint > t_min {
+                let t: f32 = hitpoint;
+                let p: Vec3 = r.point_at_parameter(t);
+                let normal: Vec3 = &(&p - &self.center) / self.radius;
+                if discriminant < 0.0005 {
+                    return Some(HitRecord{t, p, normal, on_edge: true});
+                }
+                else {
+                    return Some(HitRecord{t, p, normal, on_edge: false});
+                }
+            }
+        }
+        None
+    }
+}
+
+pub struct HitableList {
+    pub list: Vec<Box<dyn Hitable>>
+}
+
+impl Hitable for HitableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut closest_so_far = t_max;
+        let mut temp_rec: Option<HitRecord> = None;
+
+        for hitable in &self.list {
+            if let Some(hit_record) = hitable.hit(r, t_min, closest_so_far) {
+                closest_so_far = hit_record.t;
+                temp_rec = Some(hit_record)
+            }
+        }
+        temp_rec
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,31 @@
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::f32::MAX;
 
 mod vec;
 mod ray;
-use crate::vec::{Vec3, dot};
+mod hitable;
+use crate::vec::{Vec3};
 use crate::ray::Ray;
+use crate::hitable::{Sphere, HitRecord, Hitable, HitableList};
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> (bool, f32) {
-    let oc = &r.origin - &center; // origin coordinate
-    let a = dot(&r.direction, &r.direction);
-    let b = 2.0 * dot(&oc, &r.direction);
-    let c = dot(&oc, &oc) - radius*radius;
-    let discriminant = b*b - 4_f32*a*c;
-    if discriminant < 0_f32 {
-        (false, -1.0) // we ray doesn't hit the sphere, no matter how negative is the discriminant
-    }
-    else {
-        let hitpoint_1 = (-b - discriminant.sqrt()) / (2.0 * a);
-        let hitpoint_2 = (-b + discriminant.sqrt()) / (2.0 * a);
-        // for checking for 0,1,2 solutions see: https://stackoverflow.com/q/15398427
-        if discriminant < 0.0005 {
-            // ray hits the sphere at a single point --> the "edge"
-            (true, hitpoint_1)
-        }
-        else {
-            // ray hits the sphere somewhere that is not the "edge"
-            (false, hitpoint_1)
-        }
-    }
-}
-
-fn color(r: &Ray) -> Vec3 {
-    let sphere_center = Vec3::new(0.0, 0.0, -1.0);
-    let (edge, t) = hit_sphere(&sphere_center, 0.5, r); // the exact location of the hit on the surface of the sphere
-    if t > 0.0 {
-        if edge {
-            // paint the edge red
+fn color(r: &Ray, world: &HitableList) -> Vec3 {
+    if let Some(hit_record) = world.hit(r, 0.0, MAX) {
+        if hit_record.on_edge {
+            // we are hitting the visible edge of the sphere, so paint it red
             return Vec3::new(1.0, 0.0, 0.0);
         }
         else {
-            let n: Vec3 = (&r.point_at_parameter(t) - &sphere_center).unit_vector();
-            return 0.5 * &Vec3::new(n.x()+1.0, n.y()+1.0, n.z()+1.0);
+            return 0.5 * &Vec3::new(hit_record.normal.x()+1.0,
+                                    hit_record.normal.y()+1.0,
+                                    hit_record.normal.z()+1.0);
         }
     }
-    let unit_direction: Vec3 = r.direction.unit_vector();
-    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
-    &((1.0_f32 - t) * &Vec3::new(1.0, 1.0, 1.0)) + &(t * &Vec3::new(0.5, 0.7, 1.0))
+    else {
+        let unit_direction: Vec3 = r.direction.unit_vector();
+        let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+        &((1.0_f32 - t) * &Vec3::new(1.0, 1.0, 1.0)) + &(t * &Vec3::new(0.5, 0.7, 1.0))
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -58,8 +38,6 @@ fn main() -> std::io::Result<()> {
     let nx: i16 = 200;
     let ny: i16 = 100;
 
-    // let col = Vec3::new(1.0, 1.0, 1.0);
-
     f.write_all("P3\n".as_bytes());
     f.write_all((format!("{} {}\n", nx, ny)).as_bytes());
     f.write_all((format!("255\n")).as_bytes());
@@ -67,12 +45,18 @@ fn main() -> std::io::Result<()> {
     let horizontal = Vec3::new(4.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
+    let world = HitableList{list: vec![
+        Box::new(Sphere{center: Vec3::new(0.0, 0.0, -1.0), radius: 0.5}),
+        Box::new(Sphere{center: Vec3::new(0.0, -100.5, -1.0), radius: 100.0})
+    ]};
     for j in (0..ny).rev() {
         for i in (0..nx) {
             let u = i as f32 / nx as f32;
             let v = j as f32 / ny as f32;
             let r = Ray::new(origin, &lower_left_corner + &(&(u*&horizontal) + &(v*&vertical)));
-            let col = color(&r);
+
+            let p = r.point_at_parameter(2.0);
+            let col = color(&r, &world);
             let ir: i16 = (255.99 * col.e[0]) as i16;
             let ig: i16 = (255.99 * col.e[1]) as i16;
             let ib: i16 = (255.99 * col.e[2]) as i16;
