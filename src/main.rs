6c289a7581bc19afae0b76edb1e79e1a4a806f42
Balázs Dukai <balazs.dukai@gmail.com>
2020-04-13 21:30:48 +0200

    Chapter 6

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::f32::MAX;
+use rand::prelude::*;
 
 mod vec;
 mod ray;
 mod hitable;
+mod camera;
 use crate::vec::{Vec3};
 use crate::ray::Ray;
 use crate::hitable::{Sphere, HitRecord, Hitable, HitableList};
+use crate::camera::Camera;
 
 fn color(r: &Ray, world: &HitableList) -> Vec3 {
     if let Some(hit_record) = world.hit(r, 0.0, MAX) {
@@ -37,26 +40,30 @@ fn main() -> std::io::Result<()> {
         .open("basic.ppm")?;
     let nx: i16 = 200;
     let ny: i16 = 100;
+    let ns: i16 = 100;
 
     f.write_all("P3\n".as_bytes());
     f.write_all((format!("{} {}\n", nx, ny)).as_bytes());
     f.write_all((format!("255\n")).as_bytes());
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
+
     let world = HitableList{list: vec![
         Box::new(Sphere{center: Vec3::new(0.0, 0.0, -1.0), radius: 0.5}),
         Box::new(Sphere{center: Vec3::new(0.0, -100.5, -1.0), radius: 100.0})
     ]};
-    for j in (0..ny).rev() {
-        for i in (0..nx) {
-            let u = i as f32 / nx as f32;
-            let v = j as f32 / ny as f32;
-            let r = Ray::new(origin, &lower_left_corner + &(&(u*&horizontal) + &(v*&vertical)));
+    let cam = Camera::new();
 
-            let p = r.point_at_parameter(2.0);
-            let col = color(&r, &world);
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let mut col = Vec3::new(0.0, 0.0, 0.0);
+            for k in 0..ns {
+                let mut rng = rand::thread_rng();
+                let u = (i as f32 + rng.gen::<f32>()) / nx as f32;
+                let v = (j as f32 + rng.gen::<f32>()) / ny as f32;
+                let r = cam.get_ray(u, v);
+                let p = r.point_at_parameter(2.0);
+                col += color(&r, &world);
+            }
+            col = &col / ns as f32;
             let ir: i16 = (255.99 * col.e[0]) as i16;
             let ig: i16 = (255.99 * col.e[1]) as i16;
             let ib: i16 = (255.99 * col.e[2]) as i16;
